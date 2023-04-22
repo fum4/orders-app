793547be312c421d424a2186178baf2ad928bae2
Isis☕Caffe <52106389+fum4@users.noreply.github.com>
2023-04-23 00:05:28 +0300

    test deploy fail

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -6,33 +6,35 @@ import OrdersList from "~/components/orders-list";
 import { db } from "~/firebase";
 
 export const useOrders = routeLoader$((requestEvent) => {
-  (async() => {
-    // const userId = requestEvent.cookie.get('userId')?.value;
-    //
-    // if (userId) {
-    //   const userSnapshot = await getDoc(doc(db, 'users', userId));
-    //
-    //   if (userSnapshot.exists()) {
-    //     const { token } = userSnapshot.data();
-    //
-    //     if (requestEvent.cookie.get('token')?.value === token) {
-    //       const ordersSnapshot = await query(
-    //         collection(db, 'orders'),
-    //         where('completed', '==', false)
-    //       );
-    //       const ordersDocs = await getDocs(ordersSnapshot);
-    //
-    //       return ordersDocs.docs.map((doc) => ({
-    //         id: doc.id,
-    //         data: doc.data()
-    //       }))
-    //     }
-    //   }
-    // }
-    //
-    // return requestEvent.redirect(302, '/auth');
-    return;
-  })()
+  // (async() => {
+  //   // const userId = requestEvent.cookie.get('userId')?.value;
+  //   //
+  //   // if (userId) {
+  //   //   const userSnapshot = await getDoc(doc(db, 'users', userId));
+  //   //
+  //   //   if (userSnapshot.exists()) {
+  //   //     const { token } = userSnapshot.data();
+  //   //
+  //   //     if (requestEvent.cookie.get('token')?.value === token) {
+  //   //       const ordersSnapshot = await query(
+  //   //         collection(db, 'orders'),
+  //   //         where('completed', '==', false)
+  //   //       );
+  //   //       const ordersDocs = await getDocs(ordersSnapshot);
+  //   //
+  //   //       return ordersDocs.docs.map((doc) => ({
+  //   //         id: doc.id,
+  //   //         data: doc.data()
+  //   //       }))
+  //   //     }
+  //   //   }
+  //   // }
+  //   //
+  //   // return requestEvent.redirect(302, '/auth');
+  //   return;
+  // })()
+
+  return;
 });
 
 const useSignOut = routeAction$((_, { cookie, redirect }) => {
